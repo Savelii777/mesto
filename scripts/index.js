13f358b282ce7e84838d70cdedac28ca88da073f
Savelii777 <sporyshev.savelii@gmail.com>
2022-08-13 16:58:53 +0300

    bugfix: исправить разметку и js

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,40 @@
-let edit_button = document.querySelector('.profile__edit-button');
+let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let popup_inputs = document.querySelector('.popup__inputs');
-let popup_input = document.querySelectorAll('.popup__input');
-let input_name = (document.querySelector('.profile__name')).textContent;
-let input_job = (document.querySelector('.profile__job')).textContent;
-let close_button = document.querySelector('.popup__close-button');
-let like_button = document.querySelectorAll('.elements__like-button');
+let popupInputs = document.querySelector('.popup__inputs');
+let inputName = document.querySelector('.popup__input_type_name');
+let inputJob = document.querySelector('.popup__input_type_job');
+let closeButton = document.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+// let like_button = document.querySelectorAll('.element__like-button');
 
 
-edit_button.addEventListener('click', showPopup);
+editButton.addEventListener('click', showPopup);
 
-like_button.forEach(like => {
-  like.addEventListener('click', (event) => {
-    event.target.classList.toggle('elements__like-button_active'); //решил для разнообразия сделать через цикл форич и стрелочную функцию
-  })
-})
+// like_button.forEach(like => {
+//   like.addEventListener('click', (event) => {
+//     event.target.classList.toggle('element__like-button_active'); //решил для разнообразия сделать через цикл форич и стрелочную функцию
+//   })
+// })
 
 function saveInfo(evt) {
 
   evt.preventDefault();
-    for(let i = 0; i < popup_input.length; i++)
-    {
-    if(popup_input[i].name === "name")
-    {
-     input_name = popup_input[i].value;
-     (document.querySelector('.profile__name')).textContent = input_name;
-     console.log(input_name);
-    }else{
-        input_job = popup_input[i].value;
-        (document.querySelector('.profile__job')).textContent = input_job;
-     console.log(input_job);
-    }
-    }
+
+    profileName.textContent = inputName.value
+    profileJob.textContent = inputJob.value
     popup.classList.remove('popup_opened');
     
   }
 
 function showPopup() {
     popup.classList.add('popup_opened');
-    popup_inputs.addEventListener('submit', saveInfo);
-    close_button.addEventListener('click', closePopup);
+    popupInputs.addEventListener('submit', saveInfo);
+    closeButton.addEventListener('click', closePopup);
   }
   function closePopup() {
-    for(let i = 0; i < popup_input.length; i++)
-    {
-    if(popup_input[i].name === "name")
-    {
-     popup_input[i].value = input_name;
-     console.log(popup_input[i].value);
-    }else{
-     popup_input[i].value = input_job;
-     console.log(popup_input[i].value);
-    }
-    }
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
     popup.classList.remove('popup_opened');
   }
 
